7f93c05bf33f6e907bf20eaa0eefde5796abd5a6
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2022-09-08 15:54:00 -0300

    Agrega Jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    verbose: true,
+    rootDir: "src",
+    collectCoverage: true,
+    coverageDirectory: "../coverage/",
+    testPathIgnorePatterns: ["/node_modules/", ".*fixture.js"],
+    coveragePathIgnorePatterns: ["/node_modules/", ".*fixture.js"],
+  };
+
+  
