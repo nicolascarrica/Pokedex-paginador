533e9b8ac0a691bd2e5bb8fd575520a75e9b37c9
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2022-10-05 12:36:55 -0300

    jest
    modifique el coverage directory pq no funcionaba como estaba

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     verbose: true,
     rootDir: "src",
     collectCoverage: true,
-    coverageDirectory: "../coverage/",
+    coverageDirectory: "/coverage/",
     testPathIgnorePatterns: ["/node_modules/", ".*fixture.js"],
     coveragePathIgnorePatterns: ["/node_modules/", ".*fixture.js"],
   };
