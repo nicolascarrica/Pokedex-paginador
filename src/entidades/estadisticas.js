3b85064b95a819fcbd74d5306df32f8a3c5e7b66
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2022-10-05 12:36:07 -0300

    Entidades
    crea objetos para mejorar la manera de conseguir la informacion de la API sin desparramar la logica de la APi por todo el codigo

---
+++ src/entidades/estadisticas.js
@@ -0,0 +1,12 @@
+export default class Estadisticas {
+    /**
+     *
+     * @param {String} nombre
+     * @param {String} base
+     */
+
+    constructor(nombre, base) {
+      this.nombre = nombre;
+      this.base = base;
+    }
+  }
---
+++ src/entidades/pokemon.js
@@ -0,0 +1,24 @@
+/**
+ * @typedef {import('./estadisticas').default} Estadisticas
+ */
+ 
+
+
+export default class Pokemon {
+    /**
+     * @param {Number} id
+     * @param {String} nombre
+     * @param {String} foto
+     * @param {Array<Estadisticas>} estadisticas
+     * @param {Array<String>} tipos
+     */
+    
+    constructor(id, nombre, foto, estadisticas = [], tipos = []) {
+      this.id = id;
+      this.nombre = nombre;
+      this.foto = foto;
+      this.estadisticas = estadisticas;
+      this.tipos = tipos;
+    }
+  }
+  
