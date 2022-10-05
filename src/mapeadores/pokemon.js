18f3ec65c213d60cda801391f52882bdf327ac40
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2022-10-05 12:40:55 -0300

    crea mapeadores
    se crean los mapeadores  que toman los datos de la API y devuelve nuevos objetos de la forma que a mi me sirve

---
+++ src/mapeadores/pokemon.js
@@ -0,0 +1,53 @@
+import Pokemon from '../entidades/pokemon.js';
+import Estadisticas from '../entidades/estadisticas.js';
+import ListadoPokemones from '../entidades/listadoPokemones.js'
+
+
+
+/**
+ * @param {Object} datosApi
+ * @returns {Pokemon}
+ */
+ export function mapearPokemon(datosApi) {
+    const {
+      id,
+      name: nombre,
+      sprites: { front_default: fotoPrincipal },
+      stats: estadisticas,
+      types: tipos,
+      
+      
+    } = datosApi;
+  
+    return new Pokemon(
+      id,
+      nombre,
+      fotoPrincipal,
+      estadisticas.map((item) => new Estadisticas(
+        item.stat.name,
+        item.base_stat,
+      )),
+      tipos.map((item) => item.type.name),
+    
+    );
+  }
+  
+  /**
+   * @param {Object} datosApi
+   * @returns {ListadoPokemones}
+   */
+  export function mapearListadoPokemones(datosApi) {
+    const {
+      count: numeroPagina,
+      next: siguienteUrl,
+      previous: anteriorUrl,
+      results: resultados,
+    } = datosApi;
+  
+    return new ListadoPokemones(
+    numeroPagina,
+      siguienteUrl,
+      anteriorUrl,
+      resultados.map((pokemon) => pokemon.name),
+    );
+  }
