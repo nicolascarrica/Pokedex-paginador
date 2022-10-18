a1c934ed08c97ea54c3404f756178dc93589c1de
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2022-10-18 18:41:54 -0300

    Agrega Babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-    presets: [
-      ['@babel/preset-env', {targets: {node: 'current'}}],
-      '@babel/preset-typescript',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
     ],
-  };
+  ],
+};
+
+//jest corre, primero pasa los archivos de .js por babel, que generan archivos de js con js adecuado para node,
+// y luego corre los tests sobre el js optimizado para node
